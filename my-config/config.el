ab7b89f3708cdc83306fc7054b7073d726e3e77e
HEYUN <heyun@HEYUNdeMacBook-Air.local>
2016-06-04 16:53:18 +0800

    1.add graphviz-dot-mode-hook

--- my-config/config.el
+++ my-config/config.el
@@ -2,7 +2,11 @@
 '("GNU/Emacs - [ " (buffer-file-name "%f \]"
                                      (dired-directory dired-directory "%b \]"))))
 (global-hl-line-mode -1)
-
+(defcustom graphviz-dot-mode-hook nil
+  "Hook run after graphviz-dot-mode is loaded"
+  :type 'hook
+  :group 'graphviz-dot-mode)
 (add-hook 'verilog-mode-hook 'global-auto-complete-mode)
 (add-to-list 'auto-mode-alist '("\\.log\\'" . compilation-mode))
 (global-set-key (kbd "C-<SPC>") 'avy-goto-char)
+(add-hook 'graphviz-dot-mode-hook 'smartparens-mode)
