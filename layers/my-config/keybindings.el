e212c806fe0988044a177c91283c293465302922
heyun <heyun@imagedesign.com.cn>
2017-05-03 16:55:08 +0800

    add key-binding of highlight-symbols-remove-all to 'spc h H'

--- layers/my-config/keybindings.el
+++ layers/my-config/keybindings.el
@@ -1,4 +1,5 @@
 ;; (global-set-key [(control f3)] 'highlight-symbol-at-point)
 ;; (global-set-key (kbd "C-<SPC>") 'avy-goto-char)
 (spacemacs/set-leader-keys "hh" 'highlight-symbol-at-point)
+(spacemacs/set-leader-keys "hH" 'highlight-symbol-remove-all)
 ;; (spacemacs/set-leader-keys "g C-s" 'svn-global-keymap)
