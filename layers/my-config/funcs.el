9702419979760b28a24f789c68375f5b21fe55ac
heyun <heyun@imagedesign.com.cn>
2017-10-18 13:26:06 +0800

    update my-project-dir-local-init

--- layers/my-config/funcs.el
+++ layers/my-config/funcs.el
@@ -146,12 +146,14 @@ find the errors."
           (insert "
 ((verilog-mode . (
                   (eval .
-                        (setq verilog-library-files
-                              (directory-files-recursively
-                                            (concat (projectile-project-root) \"digital/rtl\") \"\\.[s]?v$\"))
+                        (mapcar
+                         (lambda (file)
+                           (add-to-list 'verilog-library-directories (file-name-directory file)))
+                         (directory-files-recursively
+                          (concat (projectile-project-root) \"digital/rtl\") \"\\.[s]?v$\")
+                         )
                         ))
-               )
- )
+               ))
 ")
           (save-buffer)
           (kill-buffer)
