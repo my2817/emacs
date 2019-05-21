407cdfe427e84a62160d423e13375862e141eefa
heyun <my2817@163.com>
2019-05-21 13:51:43 +0800

    [funcs] add my-yank-image-from-win-clipboard-through-powershell

--- layers/my-config/funcs.el
+++ layers/my-config/funcs.el
@@ -357,3 +357,16 @@ and return as PATH-to-FILE::Line-Number."
                       (replace-regexp-in-string "[xXhH]" "" (thing-at-point 'word 'no-properties)))))
     (message (format "dex2hex : %s -> %d" local-word (string-to-number  local-word 16))) )
   )
+
+(defun my-yank-image-from-win-clipboard-through-powershell()
+  "to simplify the logic, use c:/Users/Public as temporary directoy, the move it into current directoy"
+  (interactive)
+  (let* ((powershell "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe")
+         (file-name (format-time-string "screenshot_%Y%m%d_%H%M%S.png"))
+         ;; (file-path-powershell (concat "c:/Users/\$env:USERNAME/" file-name))
+         (file-path-wsl (concat "./images/" file-name))
+         )
+    ;; (shell-command (concat powershell " -command \"(Get-Clipboard -Format Image).Save(\\\"C:/Users/\\$env:USERNAME/" file-name "\\\")\""))
+    (shell-command (concat powershell " -command \"(Get-Clipboard -Format Image).Save(\\\"C:/Users/Public/" file-name "\\\")\""))
+    (rename-file (concat "/mnt/c/Users/Public/" file-name) file-path-wsl)
+    ))
